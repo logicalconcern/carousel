cb111d5e74cc84cdce73d4400f9ca11aad39b5a8
John Gardiner <john.gardiner@algomi.com>
2019-02-06 01:00:23 +0000

    [SAP-002] wire initial components

---
+++ src/main/components/Application/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from '../Carousel';
+import Header from '../Header';
+import styles from "./index.css";
+
+export default () => {
+    return (
+        <div className={styles.application}>
+            <Header>Carousel Test</Header>
+        </div>
+    );
+};
