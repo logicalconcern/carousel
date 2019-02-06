1aaf55a4664c8d55602b0571469d74d42a5b4cf9
John Gardiner <john.gardiner@algomi.com>
2019-02-06 04:16:13 +0000

    [SAP-002] implement Carousel without animation

---
+++ src/main/components/Carousel/Item/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LazyLoad from 'react-lazyload';
+import styles from "./index.css";
+
+export default ({ data, index, items, ordinal }) => {
+  // console.log(data.likes, data.user)
+    const target = ( index + 1 ) + ordinal
+    const actual = target > items ? target - items : target
+    const order = { order: actual }
+    return (
+      <li className={styles.item} style={order}>
+          <LazyLoad height={320} >
+              <img width="100%" height={320} src={data.largeImageURL} />
+          </LazyLoad>
+      </li>
+    )
+}
